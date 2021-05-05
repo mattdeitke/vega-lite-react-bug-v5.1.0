21a77eaef026c754aab883235fd319b9bbfc41db
Matt Deitke <mdeitke23@gmail.com>
2021-05-05 13:35:18 -0700

    add minimal example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { VegaLite } from "react-vega";
+
+const spec = {
+  width: 400,
+  height: 200,
+  mark: "bar",
+  encoding: {
+    x: { field: "a", type: "ordinal" },
+    y: { field: "b", type: "quantitative" },
+  },
+  data: { name: "table" }, // note: vega-lite data attribute is a plain object instead of an array
+};
+
+const barData = {
+  table: [
+    { a: "A", b: 28 },
+    { a: "B", b: 55 },
+    { a: "C", b: 43 },
+    { a: "D", b: 91 },
+    { a: "E", b: 81 },
+    { a: "F", b: 53 },
+    { a: "G", b: 19 },
+    { a: "H", b: 87 },
+    { a: "I", b: 52 },
+  ],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <VegaLite spec={spec} data={barData} />;
 }
 
 export default App;
